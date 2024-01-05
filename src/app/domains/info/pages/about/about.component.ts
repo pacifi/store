0b506ce4e3417feb641066d1a408a15992c73cf7
bonnier.mamani <bonnier.mamani@lim.upeu.edu.pe>
2024-01-04 21:44:30 -0500

    directivas

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,13 +1,15 @@
-import {Component, signal, WritableSignal} from '@angular/core';
-import {CounterComponent} from "../../../shared/components/counter/counter.component";
+import { Component, signal, WritableSignal } from '@angular/core';
+import { CounterComponent } from "@shared/components/counter/counter.component";
 import { WaveAudioComponent } from "../../components/wave-audio/wave-audio.component";
+import { HighlightDirective } from "@shared/directives/highlight.directive";
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [
     CounterComponent,
-    WaveAudioComponent
+    WaveAudioComponent,
+    HighlightDirective
   ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
