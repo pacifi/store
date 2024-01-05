02e3130c961e69f8a513b400fd01525f7d98f719
bonnier.mamani <bonnier.mamani@lim.upeu.edu.pe>
2024-01-05 08:33:11 -0500

    pass angular 17

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, inject, signal } from '@angular/core';
 import { ProductService } from "@shared/services/product.service";
 import { ProductModels } from "@shared/models/product.models";
-import { CurrencyPipe, UpperCasePipe } from "@angular/common";
+import { CurrencyPipe, NgForOf, UpperCasePipe } from "@angular/common";
 import { CartService } from "@shared/services/cart.service";
 
 @Component({
