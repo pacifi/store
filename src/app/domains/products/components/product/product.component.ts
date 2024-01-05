19318eb0b47599541123630bdd02de65a16e5e87
bonnier.mamani <bonnier.mamani@lim.upeu.edu.pe>
2024-01-04 22:32:34 -0500

    detalle producto

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -3,6 +3,7 @@ import { ProductModels } from "../../../shared/models/product.models";
 import { CurrencyPipe, DatePipe, UpperCasePipe } from "@angular/common";
 import { ReversePipe } from "@shared/pipes/reverse.pipe";
 import { TimeAgoPipe } from "@shared/pipes/time-ago.pipe";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-product',
@@ -12,7 +13,8 @@ import { TimeAgoPipe } from "@shared/pipes/time-ago.pipe";
     CurrencyPipe,
     DatePipe,
     ReversePipe,
-    TimeAgoPipe
+    TimeAgoPipe,
+    RouterLink
   ],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
